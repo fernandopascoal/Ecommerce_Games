369813ebc4390c71f188bfe217b627fec87614e2
Fernando Pascoal <fernandodevpascoal@gmail.com>
2021-08-24 23:10:54 -0300

    Criaçao do projeto inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Games from './components/Games';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Games_Content'>
+        <Games className='Games.triple' title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+      </div>
     </div>
   );
 }
