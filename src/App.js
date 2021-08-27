388698fa8fc1ff50640ca1fae3f191c69a3e590a
Fernando Pascoal <fernandodevpascoal@gmail.com>
2021-08-26 21:38:05 -0300

    Criaçao dos cards de games e do topo do site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-
+import React, { useState, useEffect, Fragment } from 'react';
 import './App.css';
-import Games from './components/Games';
 
-function App() {
+import './components/Products.css'
+import Header from './components/Header';
+
+
+function Products(){
+  const [products, setProducts] = useState([])
+  const UrlApi = "products.json";
+  
+  useEffect(() => {
+    loadProducts();
+  },[]);
+
+  const loadProducts = async () => {
+    const response = await fetch(UrlApi)
+    const data = await response.json();
+    setProducts(data);
+    
+  }
+
+  console.log(products)
+
+  return (
+    <Fragment>
+      {products.map((item) => (
+        <div className='products_container'>
+            <div className='products_info' >
+                <h2 className='title'>{item.name}</h2>
+                <p className='price'>R$ {item.price.toFixed(2)}</p>
+                <button className='btn'>Adicionar ao Carrinho</button>
+            </div>
+            <img src={item.image} alt=""  className='product_img'/>
+        </div>
+     ))}
+    </Fragment>
+  )
+}
+
+
+export default function App() {
   return (
     <div className="App">
+      <Header/>
       <div className='Games_Content'>
-        <Games className='Games.triple' title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
-        <Games title= 'GTA' img = '###' description = '#####################.' price = '49,90'></Games>
+        <Products />
       </div>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+export default () => {
+    return (
+        <header className='header'>
+            <div >
+                <h1>Games.com</h1>
+                <p>A sua loja de games na internet</p>
+            </div>
+        </header>
+    )
+}
